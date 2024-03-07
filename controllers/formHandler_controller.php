3c98e50046e17f01dc69228d3b2183b278ca1045
emilie <emilie.le-lan@epitech.eu>
2024-03-07 15:52:58 +0100

    add controller for identify submitted form

---
+++ controllers/formHandler_controller.php
@@ -0,0 +1,42 @@
+<?php
+
+include 'authentificator_controller.php';
+include 'validator_controller.php';
+include 'hashing_controller.php';
+header('Content-Type: application/json; charset=utf-8');
+
+
+
+class FormHandler {
+    
+    private $authentificator;
+
+    public function __construct($authentificator) {
+        $this->authentificator = $authentificator;
+    }
+
+    // Méthode d'identification du formulaire soumis
+    public function selectForm($formData) {
+        if (isset($formData['form_type']) && $formData['form_type'] === 'register') {
+            $this->authentificator->register($formData);
+            
+        } elseif (isset($formData['form_type']) && $formData['form_type'] === 'login') {
+            $this->authentificator->login($formData);
+       
+        } else {
+            $response = [
+                'status' => 'error',
+                'message' => 'Formulaire non trouvé'
+            ];
+            echo json_encode($response);
+        }
+    }
+}
+
+$hashing = new Hashing();
+$validator = new Validator($validatorModel);
+$authentificator = new Authentificator($validator, $hashing, $authModel);
+$formHandler = new FormHandler($authentificator);
+$formHandler->selectForm($_POST);
+
+?>
