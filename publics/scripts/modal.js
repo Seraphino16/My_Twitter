5b356fc6c1d233353db3dd61dd64ae7fac3991d3
emilie <emilie.le-lan@epitech.eu>
2024-03-07 15:31:36 +0100

    add for modal control and automatic page refresh

---
+++ publics/scripts/modal.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+    
+    $('#modalRegister').click(function() {
+    
+        $.get('../../views/register.html', function(data) {
+        
+            $('#inscriptionModalContent').html(data);
+            $('#inscriptionModal').modal('show');
+
+            $('#inscriptionModal').on('hidden.bs.modal', function () {
+                location.reload();
+            });
+        });
+    });
+
+
+    $('#modalLogin').click(function() {
+        
+        $.get('../../views/login.html', function(data) {
+
+            $('#connexionModalContent').html(data);
+            $('#connexionModal').modal('show');
+        });
+
+        $('#connexionModal').on('hidden.bs.modal', function () {
+            location.reload();
+        });
+    });
+
+
+});
