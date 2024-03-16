53661f0f61c82d1661067e41883b05bbb087df31
Séraphin <seraphinbnt@gmail.com>
2024-03-16 14:25:11 +0100

    remove comments and console.log

--- publics/scripts/profil.js
+++ publics/scripts/profil.js
@@ -17,35 +17,12 @@ $(document).ready(function() {
     } else {
         $("#edit-profile-btn").remove();
     }
-
-    getConnectedUserFollows(id, username);
-
-    // if(selectedUsername !== username) {
-    //     if(isFollowed) {
-    //         displayUnfollowBtn();
-    //     } else {
-    //         displayFollowBtn();
-    //     }
-    // } else {
-    //    console.log("Edit profile");
-    // }
     
     if (fullname && username) {
-     
-        // $('.fullname').html('<strong>' + fullname + '</strong>');
-        // $('.username').text('@' + username);
-
-        // $('.fullname').text(fullname);
-        // $('.pseudo').text('@' + username);
-
     
         getUserInfos(id, selectedUsername);
         getUserFollowers(id, selectedUsername);
         getUserFollows(id, selectedUsername);
-
-        // let ConnectedFollowsList = getUserFollows(id, username);;
-        // console.log(ConnectedFollowsList);
-
 
         getFollowsList(id, selectedUsername);
         getFollowersList(id, selectedUsername, username);
@@ -62,7 +39,6 @@ $(document).ready(function() {
             unfollow(id, username, selectedUsername);
         })
 
-        console.log($("#sessionFullname"));
         $('#sessionFullname').html('<strong>' + fullname + '</strong>');
 
         $('#sessionPseudo').text("@" + username);
@@ -88,8 +64,6 @@ function getUserInfos(id, selectedUsername) {
         success: function(response) {
             
             if (response.status === 'success') {
-
-                console.log(response);
 
                 const user = response.data;
 
@@ -128,8 +102,6 @@ function getUserInfos(id, selectedUsername) {
             } else {
                 console.error('Erreur lors de la récupération des informations supplémentaires.');
             }
-
-            
         },
         error: function(xhr, status, error) {
                 console.log('Erreur AJAX : ');
@@ -211,7 +183,6 @@ function getFollowersList(id, selectedUsername, username) {
         data: formData,
         dataType: 'json',
         success: function(data) {
-            console.log(data);
             ul = displayList(data.followersList);
             modalFollowers.appendChild(ul);
 
@@ -293,31 +264,11 @@ function updateProfile(id, selectedUsername) {
     });
 }
 
-function getConnectedUserFollows (id, username) {
-
-    const formData = {
-        id: id,
-        username: username,
-        action: "getFollowsList"
-    };
-
-    $.ajax({
-        url: "../controllers/user_controller.php",
-        method: "POST",
-        data: formData,
-        dataType: 'json',
-        success: function(data) {
-            return data.followsList;
-        }
-    });
-}
-
 function checkFollow (followers, username) {
 
     let isFollowed = false;
 
     followers.forEach(follower => {
-        console.log(follower.username);
         if(follower.username === username) {
             isFollowed = true;
         }
@@ -379,267 +330,3 @@ function unfollow (id, currentUser, userToUnfollow) {
         }
     })
 }
-
-
-
-
-
-
-/*$(document).ready(function () {
-    console.log("Hello");
-
-    let idValue  = 1;
-
-    $.ajax({
-        url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-        method: "POST",
-        data: {
-            id: idValue,
-        },
-        success: function (data) {
-            // console.log(data);
-            dataJSON = JSON.parse(data);
-            console.log(dataJSON);
-            displayUser(dataJSON[0]);
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    })
-
-    $.ajax({
-        url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-        method: "POST",
-        success: function (data) {
-            let tweets = JSON.parse(data);
-            console.log(tweets);
-        }
-    })
-})
-
-function displayUser(user) {
-    console.log(user);
-
-    let username = "@" + user.username;
-
-    $(".pseudo").text(username);
-    $(".fullname").text(user.firstname);
-
-    if(user.bio !== null && user.bio.length !== 0) {
-        $(".bio").text(user.bio);
-    }
-    
-    if(user.localisation !== null && user.localisation.length !== 0) {
-         $(".bi-geo-alt-fill").text(user.localisation);
-         $(".bi-geo-alt-fill").css("color", "gray");
-    } else {
-        $(".bi-geo-alt-fill").parent().parent().css("display", "none");
-    }
-
-    if(user.website !== null && user.website.length !== 0) {
-        $(".fa-link").css("color", "gray");
-         $(".website-url").text(user.website);
-    } else {
-        $(".website-url").parent().parent().css("display", "none");
-    }
-   
-    let joinedDate = formatJoinedDate(user.created_at);
-    $(".bi-calendar-event").text(joinedDate);
-    $(".bi-calendar-event").css("color", "gray");
-   
-
-    $.ajax({
-        url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-        method: "POST",
-        success: function (data) {
-            $(".count-following").text(data);
-        }
-    });
-
-    $.ajax({
-        url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-        success: function (data) {
-            $(".count-followers").text(data);
-        }
-    })
-
-}
-
-function formatJoinedDate (dateString) {
-
-    let newDate = new Date(dateString)
-
-    let year = newDate.getFullYear()
-    let month = newDate.toLocaleString("en-us", { month: "long"});
-
-    return " Joined " + month + " " + year;
-}
-
-$(".count-following-i").click(function () {
-    console.log("Let's get your follows !");
-
-    idValue = 16;
-
-    $.ajax({
-        url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-        method: "POST",
-        data: {
-            id: idValue,
-        },
-        success: function(data) {
-            let follows = JSON.parse(data)
-            console.log(follows);
-        },
-        error: function(data) {
-            console.log(data);$(document).ready(function () {
-                console.log("Hello");
-            
-                let idValue  = 16;
-            
-                $.ajax({
-                    url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-                    method: "POST",
-                    data: {
-                        id: idValue,
-                    },
-                    success: function (data) {
-                        // console.log(data);
-                        dataJSON = JSON.parse(data);
-                        console.log(dataJSON);
-                        console.log(JSON.stringify(data));
-                        displayUser(dataJSON[0]);
-                    },
-                    error: function (error) {
-                        console.log(error);
-                    }
-                })
-            
-                $.ajax({
-                    url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-                    method: "POST",
-                    success: function (data) {
-                        let tweets = JSON.parse(data);
-                        console.log(tweets);
-                    }
-                })
-            })
-            
-            function displayUser(user) {
-                console.log(user);
-            
-                let username = "@" + user.username;
-            
-                $(".pseudo").text(username);
-                $(".fullname").text(user.firstname);
-            
-                if(user.bio !== null && user.bio.length !== 0) {
-                    $(".bio").text(user.bio);
-                }
-                
-                if(user.localisation !== null && user.localisation.length !== 0) {
-                     $(".bi-geo-alt-fill").text(user.localisation);
-                     $(".bi-geo-alt-fill").css("color", "gray");
-                } else {
-                    $(".bi-geo-alt-fill").parent().parent().css("display", "none");
-                }
-            
-                if(user.website !== null && user.website.length !== 0) {
-                    $(".fa-link").css("color", "gray");
-                     $(".website-url").text(user.website);
-                } else {
-                    $(".website-url").parent().parent().css("display", "none");
-                }
-               
-                let joinedDate = formatJoinedDate(user.created_at);
-                $(".bi-calendar-event").text(joinedDate);
-                $(".bi-calendar-event").css("color", "gray");
-               
-            
-                $.ajax({
-                    url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-                    method: "POST",
-                    success: function (data) {
-                        $(".count-following").text(data);
-                    }
-                });
-            
-                $.ajax({
-                    url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-                    success: function (data) {
-                        $(".count-followers").text(data);
-                    }
-                })
-            
-            }
-            
-            function formatJoinedDate (dateString) {
-            
-                let newDate = new Date(dateString)
-            
-                let year = newDate.getFullYear()
-                let month = newDate.toLocaleString("en-us", { month: "long"});
-            
-                return " Joined " + month + " " + year;
-            }
-            
-            $(".count-following-i").click(function () {
-                console.log("Let's get your follows !");
-            
-                idValue = 16;
-            
-                $.ajax({
-                    url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-                    method: "POST",
-                    data: {
-                        id: idValue,
-                    },
-                    success: function(data) {
-                        let follows = JSON.parse(data)
-                        console.log(follows);
-                    },
-                    error: function(data) {
-                        console.log(data);
-                    }
-                });
-            
-            })
-            
-            $(".count-followers-i").click(function () {
-                console.log("Let's get your followers !");
-            
-                idValue = 16;
-            
-                $.ajax({
-                    url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-                    method: "POST",
-                    data: {
-                        id: idValue,
-                    },
-                    success: function(data) {
-                        let followers = JSON.parse(data);
-                        console.log(followers);
-                    }
-                })
-            })
-        }
-    });
-
-})
-
-$(".count-followers-i").click(function () {
-    console.log("Let's get your followers !");
-
-    idValue = 16;
-
-    $.ajax({
-        url: "/Webac/W-WEB-090-LIL-1-1-academie-zoe.pilia/controllers/user_controller.php",
-        method: "POST",
-        data: {
-            id: idValue,
-        },
-        success: function(data) {
-            let followers = JSON.parse(data);
-            console.log(followers);
-        }
-    });
-})*/
